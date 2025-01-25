75f34fad8c41791ca0e12be4810719508e2f97e0
sanashajiya <ssanashajiya@gmail.com>
2025-01-25 19:10:14 +0530

    AppBar styling updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:trackit/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        )),
     home: Expenses(),
   ));
 }
